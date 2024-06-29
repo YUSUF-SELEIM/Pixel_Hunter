fdce6e6c6de42e2cd500eb4f8351821a3757f604
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-06-30 00:08:10 +0300

    Added Header component

---
+++ src/app/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="w-full navbar bg-gray-100 p-2 px-4 shadow-2xl">
+            <a className="text-2xl text-black" href={'/'}>
+                Pixel<span className="font-extrabold text-cyan-500 ml-1">Hunter</span>
+            </a>
+        </div>
+    )
+}
+
+export default Header
