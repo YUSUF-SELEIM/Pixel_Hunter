263247d7b24ab099b721b3baaafc3871449c6adf
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-01 22:51:35 +0300

    pulled ImageData into a separate file

---
+++ data/ImageData.ts
@@ -0,0 +1,77 @@
+const imageData = [
+    {
+      id: '1',
+      src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/I-1.webp',
+      alt: 'A Crowd of Weird Faces I',
+      characters:
+        [
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_I/Bully.webp',
+            Name: 'Bully',
+            position: { x: 93, y: 28 },
+            found: false
+          },
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_I/Knife-Nose.webp',
+            Name: 'Knife Nose',
+            position: { x: 6, y: 16 },
+            found: false,
+          },
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_I/Potato-Head.webp',
+            Name: 'Potato Head',
+            position: { x: 80, y: 61 },
+            found: false,
+          }
+        ],
+    },
+    {
+      id: '2',
+      src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/I-3.webp',
+      alt: 'A Crowd of Weird Faces II',
+      characters:
+        [
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_II/Baby.webp',
+            Name: 'Baby',
+            position: { x: 88, y: 28 },
+            found: false,
+          },
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_II/Doggo.webp',
+            Name: 'Doggo',
+            position: { x: 41, y: 96 },
+            found: false,
+          },
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_II/Olive.webp',
+            Name: 'Olive',
+            position: { x: 75, y: 66 },
+            found: false,
+          },
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_II/Yusuf.webp',
+            Name: 'Yusuf',
+            position: { x: 21, y: 5 },
+            found: false,
+          }
+        ],
+  
+    },
+    {
+      id: '3',
+      src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/I-2.webp',
+      alt: 'Ice Creams and Lollipop',
+      characters:
+        [
+          {
+            src: 'https://wsrv.nl/?url=https://raw.githubusercontent.com/YUSUF-SELEIM/Pixel_Hunter/main/illustrations/Game_III/Cute-Lollipop.webp',
+            Name: 'Cute Lollipop',
+            position: { x: 77, y: 21 },
+            found: false,
+          },
+        ],
+    },
+  ];
+
+export default imageData;  
