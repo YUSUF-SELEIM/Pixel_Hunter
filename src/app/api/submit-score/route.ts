63b5b8002cb5df753a3148439d93f197e8c91c04
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-03 19:36:05 +0300

    Updated submit-score API route to include time taken

--- src/app/api/submit-score/route.ts
+++ src/app/api/submit-score/route.ts
@@ -1,18 +1,18 @@
-import { submitUsernameWithSession } from '@/lib/actions';
+import { submitUsernameAndTimeTaken } from '@/lib/actions';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(req: NextRequest) {
   try {
-    const { sessionID, username } = await req.json();
-    const game = await submitUsernameWithSession(sessionID, username);
+    const { sessionID, username, timeTaken } = await req.json();
+    const game = await submitUsernameAndTimeTaken(sessionID, username, timeTaken);
     return new NextResponse(
       JSON.stringify({ timeTaken: game.timeTaken, imageID: game.imageID }),
       { status: 200 }
     );
   } catch (error) {
-    console.error('Error submitting username with session ID:', error);
+    console.error('Error submitting username and Time Taken with session ID:', error);
     return new NextResponse(
-      JSON.stringify({ error: 'Could not submit username with session ID.' }),
+      JSON.stringify({ error: 'Could not submit username Time Taken with session ID.' }),
       { status: 500 }
     );
   }
