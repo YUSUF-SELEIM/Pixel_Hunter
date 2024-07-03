abc1d00d1d3f9af14df3851028d9fc73356064e8
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-04 02:09:21 +0300

    Updated GitHub link in footer component

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from 'react-icons/fa'
 function Footer() {
     return (
         <footer className="footer footer-center bg-gray-100 text-base-content p-6">
-            <a href='https://github.com/YUSUF-SELEIM'>
+            <a href='https://github.com/YUSUF-SELEIM' target=''>
                 <FaGithub className="text-3xl text-cyan-500" />
             </a>
         </footer>
