43826318c7585c5239981b516aeeb6bc05372807
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-03 19:37:50 +0300

    Update Header component to include children prop aka Timer component

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,9 +1,12 @@
-function Header() {
+import React, { ReactNode } from 'react';
+
+function Header({ children }: { children: ReactNode }) {
     return (
-        <div className="w-full navbar bg-gray-100 p-2 px-4 shadow-2xl">
+        <div className="w-full navbar bg-gray-100 p-2 px-4 shadow-2xl justify-between">
             <a className="text-2xl text-black" href={'/'}>
                 Pixel<span className="font-extrabold text-cyan-500 ml-1">Hunter</span>
             </a>
+            {children}
         </div>
     )
 }
