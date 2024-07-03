d9b723b9ea7a17909909833a9969bb995d3cbc25
YUSUF-SELEIM <yusufabdelfattah207@gmail.com>
2024-07-04 02:10:08 +0300

    Added target blank to github link

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,7 +2,7 @@ import { FaGithub } from 'react-icons/fa'
 function Footer() {
     return (
         <footer className="footer footer-center bg-gray-100 text-base-content p-6">
-            <a href='https://github.com/YUSUF-SELEIM' target=''>
+            <a href='https://github.com/YUSUF-SELEIM' target='_blank'>
                 <FaGithub className="text-3xl text-cyan-500" />
             </a>
         </footer>
